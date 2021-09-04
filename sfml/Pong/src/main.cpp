7e54562a894cc35bde853bdab8f210c6a78c76ce
chauhan739 <chauhan.araynsingh18@gmail.com>
2021-09-04 16:13:26 +0530

    major changes

--- sfml/Pong/src/main.cpp
+++ sfml/Pong/src/main.cpp
@@ -1,111 +1,137 @@
-#include <iostream>
 #include "SFML/Graphics.hpp"
-#include "SFML/Window.hpp"
 
-using namespace std;
 using namespace sf;
 
-void UserControl(RectangleShape&);
-void AI(RenderWindow&, RectangleShape&, RectangleShape&, CircleShape&);
-void Draw(RenderWindow&, RectangleShape&, RectangleShape&, CircleShape&);
+bool intersects(RectangleShape&, RectangleShape&);
 
 int main(){
-  RenderWindow window(VideoMode(600, 400), "Pong", Style::Titlebar);
+  const int width = 640;
+  const int height = 480;
+
+  RenderWindow window(VideoMode(width, height), "Pong!", Style::Default);
   window.setFramerateLimit(60);
 
-  CircleShape ball;
-  RectangleShape paddleLeft(Vector2f(5.0f, 25.0f));
-  RectangleShape paddleRight(Vector2f(5.0f, 25.0f));
+  RectangleShape paddleRight, paddleLeft, ball, upperWall, lowerWall, leftWall, rightWall;
 
-  paddleLeft.setFillColor(Color::White);
-  paddleLeft.setPosition(3.0f, window.getSize().y/2);
-  
+  // setting up paddleRight
+  paddleRight.setSize(Vector2f(10.0f, 40.0f));
   paddleRight.setFillColor(Color::White);
-  paddleRight.setPosition(592.0f, window.getSize().y/2);
+  paddleRight.setPosition(635.0f, 220.0f);
 
-  ball.setRadius(3.0f);
-  ball.setFillColor(Color::White);
-  ball.setPosition(window.getSize().x/2, window.getSize().y/2);
+  // setting up paddleLeft
+  paddleLeft.setSize(Vector2f(10.0f, 40.0f));
+  paddleLeft.setFillColor(Color::White);
+  paddleLeft.setPosition(5.0f, 220.0f);
+
+  // setting up ball
+  ball.setSize(Vector2f(10.0f, 10.0f));
+  ball.setFillColor(Color::Red);
+  ball.setPosition(width/2, height/2);
+  Vector2f ballSpeed(5.0f, 3.0f);
+
+  // setting up upperWall
+  upperWall.setSize(Vector2f(width, 3.0f));
+  upperWall.setFillColor(Color::Yellow);
+  upperWall.setPosition(0.0f, 0.0f);
+
+  // setting up lowerWall
+  lowerWall.setSize(Vector2f(width, 3.0f));
+  lowerWall.setFillColor(Color::Yellow);
+  lowerWall.setPosition(0.0f, height-3);
+
+  // setting up leftWall
+  leftWall.setSize(Vector2f(3.0f, height));
+  leftWall.setFillColor(Color::Yellow);
+  leftWall.setPosition(0.0f, 0.0f);
+
+  // setting up rightWall
+  rightWall.setSize(Vector2f(width, 3.0f));
+  rightWall.setFillColor(Color::Yellow);
+  rightWall.setPosition(width-3, 0.0f);
+
+  bool leftGoing = false, rightGoing = true, upGoing = false, downGoing = true;
 
   while(window.isOpen()){
-      Event ev;
+    Event ev;
 
-      while(window.pollEvent(ev)){
-	if(ev.type == Event::Closed) window.close();
-	if(ev.key.code == Keyboard::Escape) window.close();
-      }
+    while(window.pollEvent(ev)) if(ev.type == Event::Closed || ev.key.code == Keyboard::Escape) window.close();
 
-      UserControl(paddleLeft);
-      AI(window, paddleRight, paddleLeft, ball);
-      Draw(window, paddleLeft, paddleRight, ball);
+    // paddleLeft user defined movement
+    if(Keyboard::isKeyPressed(Keyboard::Key::Up) && paddleLeft.getPosition().y >= 3.0f) paddleLeft.move(0.0f, -4.0f);
+    else if (Keyboard::isKeyPressed(Keyboard::Key::Down) && paddleLeft.getPosition().y <= 437.0f) paddleLeft.move(0.0f, 4.0f);
+
+    // ball hitting paddleRight and paddleLeft
+    if(intersects(ball, paddleRight) && upGoing){
+      ballSpeed.x = -ballSpeed.x;
+      rightGoing = false;
+      leftGoing = true;
     }
-  
+    else if(intersects(ball, paddleRight) && downGoing){
+      ballSpeed.x = -ballSpeed.x;
+      rightGoing = false;
+      leftGoing = true;
+    }
+    else if(intersects(ball, paddleLeft) && upGoing){
+      ballSpeed.x = -ballSpeed.x;
+      rightGoing = true;
+      leftGoing = false;
+    }
+    else if(intersects(ball, paddleLeft) && downGoing){
+      ballSpeed.x = -ballSpeed.x;
+      rightGoing = true;
+      leftGoing = false;
+    }
+
+    // ball hitting upperWall and lowerWall
+    if(intersects(ball, upperWall) && leftGoing){
+      ballSpeed.y = -ballSpeed.y;
+      upGoing = false;
+      downGoing = true;
+    }
+    else if(intersects(ball, upperWall) && rightGoing){
+      ballSpeed.y = -ballSpeed.y;
+      upGoing = false;
+      downGoing = true;
+    }
+    else if(intersects(ball, lowerWall) && leftGoing){
+      ballSpeed.y = -ballSpeed.y;
+      upGoing = true;
+      downGoing = false;
+    }
+    else if(intersects(ball, lowerWall) && rightGoing){
+      ballSpeed.y = -ballSpeed.y;
+      upGoing = true;
+      downGoing = false;
+    }
+
+    // if((intersects(ball, rightWall) && upGoing) || (intersects(ball, rightWall) && downGoing)){
+    //   ballSpeed.y = -ballSpeed.y;
+    //   leftGoing = false;
+    //   rightGoing = true;
+    // }
+
+    ball.move(ballSpeed);
+
+    // paddleRight auto movement
+    if(paddleRight.getPosition().y >= 3.0f || paddleRight.getPosition().y <= (height-43)) paddleRight.setPosition(635.0f, ball.getPosition().y-20);
+    
+    window.clear(Color::Black);
+    window.draw(paddleRight);
+    window.draw(paddleLeft);
+    window.draw(ball);
+    window.draw(upperWall);
+    window.draw(lowerWall);
+    window.draw(leftWall);
+    window.draw(rightWall);
+    window.display();
+  }
+
   return 0;
 }
 
-void Draw(RenderWindow& window, RectangleShape& paddleLeft, RectangleShape& paddleRight, CircleShape& ball){
-  window.clear();
-  
-  window.draw(ball);
-  window.draw(paddleLeft);
-  window.draw(paddleRight);
+bool intersects(RectangleShape& r1, RectangleShape& r2){
+  FloatRect a = r1.getGlobalBounds();
+  FloatRect b = r2.getGlobalBounds();
 
-  window.display();
+  return a.intersects(b);
 }
-
-void UserControl(RectangleShape& paddleLeft){
-  if(Keyboard::isKeyPressed(Keyboard::Key::Up) && paddleLeft.getPosition().y >= 0.0f) paddleLeft.move(0.0f, -5.0f);
-  if(Keyboard::isKeyPressed(Keyboard::Key::Down) && paddleLeft.getPosition().y <= 375.0f) paddleLeft.move(0.0f, 5.0f);
-}
-
-void AI(RenderWindow& window, RectangleShape& paddleRight, RectangleShape& paddleLeft, CircleShape& ball){
-  // ball.move(0.0f, 1.0f);
-  
-  bool leftGoing = true;
-  bool rightGoing = false;
-  bool upGoing = false;
-  bool downGoing = true;
-
-  
-  // paddleRight automovement
-   if(paddleRight.getPosition().y >= 0.0f && paddleRight.getPosition().y <= 375.0f) paddleRight.setPosition(window.getSize().x-8, ball.getPosition().y);
-
-  // ball hitting paddleLeft
-  if((ball.getPosition().x >= paddleLeft.getPosition().x && ball.getPosition().x <= (paddleLeft.getPosition().x+25.0f)) && (ball.getPosition().y <= (paddleLeft.getPosition().y+5.0f))){
-    if(upGoing) ball.move(4.0f, -1.0f);
-    else if(downGoing) ball.move(4.0f, 1.0f);
-    rightGoing = true;
-    leftGoing = false;
-  }
-  // ball hitting paddleRight
-  else if((ball.getPosition().y + 6.0f) >= paddleRight.getPosition().y){
-    if(upGoing) ball.move(-4.0f, -1.0f);
-    else if(downGoing) ball.move(-4.0f, 1.0f);
-    rightGoing = false;
-    leftGoing = true;
-  }
-
-  // ball hitting lower wall
-  if((ball.getPosition().y >= 394.0f) && rightGoing){
-    ball.move(2.0f, 2.0f);
-    upGoing = true;
-    downGoing = false;
-  }
-  else if((ball.getPosition().y >= 394.0f) && leftGoing){
-    ball.move(-2.0f, 2.0f);
-    upGoing = true;
-    downGoing = false;
-  }
-  // ball hitting upper wall
-  else if((ball.getPosition().y <= 0.0f) && rightGoing){
-    ball.move(2.0f, -2.0f);
-    upGoing = false;
-    downGoing = true;
-  }
-  else if((ball.getPosition().y <= 0.0f) && leftGoing){
-    ball.move(-2.0f, -2.0f);
-    upGoing = false;
-    downGoing = true;
-  }
-}
-
