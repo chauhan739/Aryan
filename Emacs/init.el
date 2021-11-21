20044acf80c7e1c079b17b4db740ce2fe8f12dab
chauhan739 <chauhan.araynsingh18@gmail.com>
2021-11-21 22:16:03 +0530

    Update whole init file from scratch/v2.0

--- Emacs/init.el
+++ Emacs/init.el
@@ -1,21 +1,26 @@
+(setq inhibit-startup-message t)
 (toggle-scroll-bar -1)
 (menu-bar-mode)
 (tool-bar-mode -1)
 (global-linum-mode t)
 (global-auto-revert-mode t)
 
-(setq python-shell-interpreter "python3")
-
 (require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives
-	     '("melpa" . "https://melpa.org/packages/"))
+	     '("melpa" . "https://melpa.org/packages/")
+	     '("melpa-stable" . "https://stable.melpa.org/packages/"))
 
 (package-initialize)
 
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
+
+(use-package exec-path-from-shell :ensure t)
+(exec-path-from-shell-initialize)
+
+(use-package flycheck :ensure t :init (global-flycheck-mode))
 
 (use-package which-key
   :ensure t
@@ -25,68 +30,66 @@
   :ensure t)
 (load-theme 'material t)
 
-(use-package auto-complete
-  :ensure t
-  :init
-  (progn
-    (ac-config-default)
-    (global-auto-complete-mode t)
-    ))
-
-(use-package company
-  :ensure t
-  :config
-  (setq company-idle-delay 0)
-  (setq company-minimum-prefix-length 3)
-
-  (global-company-mode t)
-  )
-
-(use-package company-irony
-  :ensure t
-  :config
-  (add-to-list 'company-backends 'company-irony)
-
-  )
-
-(use-package irony
-  :ensure t
-  :config
-  (add-hook 'c++-mode-hook 'irony-mode)
-  (add-hook 'c-mode-hook 'irony-mode)
-  (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
-  )
-
-(use-package irony-eldoc
-  :ensure t
-  :config
-  (add-hook 'irony-mode-hook #'irony-eldoc))
-
-(use-package flycheck
-  :ensure t
-  :init (global-flycheck-mode t))
-
-(use-package jedi
-  :ensure t
-  :init
-  (add-hook 'python-mode-hook 'jedi-setup)
-  (add-hook 'python-mode-hook 'jedi:ac-setup))
-
-;; Using package dumb-jump for jumping on function definition
-(use-package dumb-jump
+(use-package company-quickhelp
   :ensure t)
 
-(use-package quickrun
-  :ensure t)
-;; After Downloading Quickrun, then execute the below command
-(require 'quickrun)
-(global-set-key [f5] (quote quickrun))
+(use-package lsp-mode :commands lsp :ensure t)
+(use-package lsp-ui :commands lsp-ui-mode :ensure t)
+;;Uncomment and execute the below code as during the time of
+;;commenting that, the reuired package was unavailable
+;;(use-package company-lsp
+;;  :ensure t
+;;  :commands company-lsp
+;;  :config (push 'company-lsp company-backends)) ;; add company-lsp as a backend
 
-;; First download lsp-java from package manager
-(require 'lsp-java)
-(add-hook 'java-mode-hook #'lsp)
+(use-package ccls
+  :ensure t
+  :config
+  (setq ccls-executable "ccls")
+  (setq lsp-prefer-flymake nil)
+  (setq-default flycheck-disabled-checkers '(c/c++-clang c/c++-cppcheck c/c++-gcc))
+  :hook ((c-mode c++-mode objc-mode) .
+         (lambda () (require 'ccls) (lsp))))
 
-;;; This will enable emacs to compile a simple cpp single file without any makefile by just pressin `F9` key
+(setq company-transformers nil company-lsp-async t company-lsp-cache-candidates nil)
+
+(use-package quickrun 
+:ensure t
+:bind ("C-c r" . quickrun))
+
+;; My config for Emacs-Java had been taken from https://github.com/neppramod/java_emacs/blob/master/emacs-configuration.org
+(use-package dap-mode
+  :ensure t
+  :after (lsp-mode)
+  :functions dap-hydra/nil
+  :config
+  (require 'dap-java)
+  :bind (:map lsp-mode-map
+         ("<f5>" . dap-debug)
+         ("M-<f5>" . dap-hydra))
+  :hook ((dap-mode . dap-ui-mode)
+    (dap-session-created . (lambda (&_rest) (dap-hydra)))
+    (dap-terminated . (lambda (&_rest) (dap-hydra/nil)))))
+
+(use-package dap-java :ensure nil)
+
+(use-package lsp-treemacs
+  :after (lsp-mode treemacs)
+  :ensure t
+  :commands lsp-treemacs-errors-list
+  :bind (:map lsp-mode-map
+         ("M-9" . lsp-treemacs-errors-list)))
+
+(use-package treemacs
+  :ensure t
+  :commands (treemacs)
+  :after (lsp-mode))
+
+(use-package lsp-java 
+:ensure t
+:config (add-hook 'java-mode-hook 'lsp))
+
+;;; This will enable emacs to compile a simple cpp single file without any makefile by just pressing [f9] key
 (defun code-compile()
   (interactive)
   (unless (file-exists-p "Makefile")
@@ -99,40 +102,15 @@
     (compile compile-command)))
 
 (global-set-key [f9] 'code-compile)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default bold shadow italic underline bold bold-italic bold])
- '(custom-enabled-themes '(wombat))
- '(fci-rule-color "#37474f")
- '(hl-sexp-background-color "#1c1f26")
- '(inhibit-startup-screen t)
+ '(ispell-dictionary nil)
  '(package-selected-packages
-   '(dumb-jump quickrun lsp-java material-theme jedi live-py-mode which-key use-package irony-eldoc flycheck company-irony auto-complete))
- '(vc-annotate-background nil)
- '(vc-annotate-color-map
-   '((20 . "#f36c60")
-     (40 . "#ff9800")
-     (60 . "#fff59d")
-     (80 . "#8bc34a")
-     (100 . "#81d4fa")
-     (120 . "#4dd0e1")
-     (140 . "#b39ddb")
-     (160 . "#f36c60")
-     (180 . "#ff9800")
-     (200 . "#fff59d")
-     (220 . "#8bc34a")
-     (240 . "#81d4fa")
-     (260 . "#4dd0e1")
-     (280 . "#b39ddb")
-     (300 . "#f36c60")
-     (320 . "#ff9800")
-     (340 . "#fff59d")
-     (360 . "#8bc34a")))
- '(vc-annotate-very-old-color nil))
+   '(lsp-java dap-mode flycheck company-quickhelp ccls company-lsp quickrun which-key material-theme use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
