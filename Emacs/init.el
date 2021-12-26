14c7d6ec2c788c5911ec35ab915fe9da86821390
chauhan739 <chauhan.araynsingh18@gmail.com>
2021-12-26 20:40:20 +0530

    Updated init.el (lsp-java)

--- Emacs/init.el
+++ Emacs/init.el
@@ -1,9 +1,14 @@
 (setq inhibit-startup-message t)
 (toggle-scroll-bar -1)
-(menu-bar-mode)
+(menu-bar-mode -1)
 (tool-bar-mode -1)
 (global-linum-mode t)
 (global-auto-revert-mode t)
+
+(setq frame-title-format
+      '((:eval (if tab-bar-mode
+                   (cdr (assq 'name (tab-bar--current-tab))) ""))
+        ": %b"))
 
 (require 'package)
 (setq package-enable-at-startup nil)
@@ -58,36 +63,72 @@
 :bind ("C-c r" . quickrun))
 
 ;; My config for Emacs-Java had been taken from https://github.com/neppramod/java_emacs/blob/master/emacs-configuration.org
-(use-package dap-mode
+;; (use-package dap-mode
+;;   :ensure t
+;;   :after (lsp-mode)
+;;   :functions dap-hydra/nil
+;;   :config
+;;   (require 'dap-java)
+;;   :bind (:map lsp-mode-map
+;;          ("<f5>" . dap-debug)
+;;          ("M-<f5>" . dap-hydra))
+;;   :hook ((dap-mode . dap-ui-mode)
+;;     (dap-session-created . (lambda (&_rest) (dap-hydra)))
+;;     (dap-terminated . (lambda (&_rest) (dap-hydra/nil)))))
+
+;; (use-package dap-java :ensure nil)
+
+;; (use-package lsp-treemacs
+;;   :after (lsp-mode treemacs)
+;;   :ensure t
+;;   :commands lsp-treemacs-errors-list
+;;   :bind (:map lsp-mode-map
+;;          ("M-9" . lsp-treemacs-errors-list)))
+
+;; (use-package treemacs
+;;   :ensure t
+;;   :commands (treemacs)
+;;   :after (lsp-mode))
+
+;; (use-package lsp-java 
+;; :ensure t
+;; :config (add-hook 'java-mode-hook 'lsp))
+
+(use-package projectile
+  :ensure t)
+(use-package flycheck
+  :ensure t)
+(use-package yasnippet
   :ensure t
-  :after (lsp-mode)
-  :functions dap-hydra/nil
-  :config
-  (require 'dap-java)
-  :bind (:map lsp-mode-map
-         ("<f5>" . dap-debug)
-         ("M-<f5>" . dap-hydra))
-  :hook ((dap-mode . dap-ui-mode)
-    (dap-session-created . (lambda (&_rest) (dap-hydra)))
-    (dap-terminated . (lambda (&_rest) (dap-hydra/nil)))))
-
-(use-package dap-java :ensure nil)
-
+  :config (yas-global-mode))
+(use-package lsp-mode
+  :ensure t
+  :hook ((lsp-mode . lsp-enable-which-key-integration))
+  :config (setq lsp-completion-enable-additional-text-edit nil))
+(use-package hydra
+  :ensure t)
+(use-package company
+  :ensure t)
+(use-package lsp-ui
+  :ensure t)
+(use-package which-key
+  :ensure t
+  :config (which-key-mode))
+(use-package lsp-java
+  :ensure t
+  :config (add-hook 'java-mode-hook 'lsp))
+(use-package dap-mode :after lsp-mode
+  :ensure t
+  :config (dap-auto-configure-mode))
+;;(use-package dap-java
+;;  :ensure t)
+(use-package helm-lsp
+  :ensure t)
+(use-package helm
+  :ensure t
+  :config (helm-mode))
 (use-package lsp-treemacs
-  :after (lsp-mode treemacs)
-  :ensure t
-  :commands lsp-treemacs-errors-list
-  :bind (:map lsp-mode-map
-         ("M-9" . lsp-treemacs-errors-list)))
-
-(use-package treemacs
-  :ensure t
-  :commands (treemacs)
-  :after (lsp-mode))
-
-(use-package lsp-java 
-:ensure t
-:config (add-hook 'java-mode-hook 'lsp))
+  :ensure t)
 
 ;;; This will enable emacs to compile a simple cpp single file without any makefile by just pressing [f9] key
 (defun code-compile()
@@ -110,7 +151,7 @@
  ;; If there is more than one, they won't work right.
  '(ispell-dictionary nil)
  '(package-selected-packages
-   '(lsp-java dap-mode flycheck company-quickhelp ccls company-lsp quickrun which-key material-theme use-package)))
+   '(lsp-mode dap-java projectile lsp-java dap-mode flycheck company-quickhelp ccls company-lsp quickrun which-key material-theme use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
