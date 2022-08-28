4d544d35f70f55235d5979c45ac7098d054da0d6
chauhan739 <chauhan.aryansingh18@gmail.com>
2022-08-28 15:09:57 +0530

    Updated treemacs

--- Emacs/init.el
+++ Emacs/init.el
@@ -25,7 +25,9 @@
 (use-package exec-path-from-shell :ensure t)
 (exec-path-from-shell-initialize)
 
-(use-package flycheck :ensure t :init (global-flycheck-mode))
+(use-package flycheck
+  :ensure t
+  :init (global-flycheck-mode))
 
 (use-package which-key
   :ensure t
@@ -40,12 +42,6 @@
 
 (use-package lsp-mode :commands lsp :ensure t)
 (use-package lsp-ui :commands lsp-ui-mode :ensure t)
-;;Uncomment and execute the below code as during the time of
-;;commenting that, the reuired package was unavailable
-;;(use-package company-lsp
-;;  :ensure t
-;;  :commands company-lsp
-;;  :config (push 'company-lsp company-backends)) ;; add company-lsp as a backend
 
 (use-package ccls
   :ensure t
@@ -59,41 +55,10 @@
 (setq company-transformers nil company-lsp-async t company-lsp-cache-candidates nil)
 
 (use-package quickrun 
-:ensure t
-:bind ("C-c r" . quickrun))
+  :ensure t
+  :bind ("C-c r" . quickrun))
 
-;; My config for Emacs-Java had been taken from https://github.com/neppramod/java_emacs/blob/master/emacs-configuration.org
-;; (use-package dap-mode
-;;   :ensure t
-;;   :after (lsp-mode)
-;;   :functions dap-hydra/nil
-;;   :config
-;;   (require 'dap-java)
-;;   :bind (:map lsp-mode-map
-;;          ("<f5>" . dap-debug)
-;;          ("M-<f5>" . dap-hydra))
-;;   :hook ((dap-mode . dap-ui-mode)
-;;     (dap-session-created . (lambda (&_rest) (dap-hydra)))
-;;     (dap-terminated . (lambda (&_rest) (dap-hydra/nil)))))
-
-;; (use-package dap-java :ensure nil)
-
-;; (use-package lsp-treemacs
-;;   :after (lsp-mode treemacs)
-;;   :ensure t
-;;   :commands lsp-treemacs-errors-list
-;;   :bind (:map lsp-mode-map
-;;          ("M-9" . lsp-treemacs-errors-list)))
-
-;; (use-package treemacs
-;;   :ensure t
-;;   :commands (treemacs)
-;;   :after (lsp-mode))
-
-;; (use-package lsp-java 
-;; :ensure t
-;; :config (add-hook 'java-mode-hook 'lsp))
-
+;; Config for lsp-java
 (use-package projectile
   :ensure t)
 (use-package flycheck
@@ -111,25 +76,134 @@
   :ensure t)
 (use-package lsp-ui
   :ensure t)
-(use-package which-key
-  :ensure t
-  :config (which-key-mode))
 (use-package lsp-java
   :ensure t
   :config (add-hook 'java-mode-hook 'lsp))
 (use-package dap-mode :after lsp-mode
   :ensure t
   :config (dap-auto-configure-mode))
-;;(use-package dap-java
-;;  :ensure t)
 (use-package helm-lsp
   :ensure t)
 (use-package helm
   :ensure t
   :config (helm-mode))
-(use-package lsp-treemacs
-  :ensure t)
 (setq dap-java-java-command "xterm -e java")
+
+;; treemacs installation from https://github.com/Alexander-Miller/treemacs#installation
+(use-package treemacs
+  :ensure t
+  :defer t
+  :init
+  (with-eval-after-load 'winum
+    (define-key winum-keymap (kbd "M-0") #'treemacs-select-window))
+  :config
+  (progn
+    (setq treemacs-collapse-dirs                   (if treemacs-python-executable 3 0)
+          treemacs-deferred-git-apply-delay        0.5
+          treemacs-directory-name-transformer      #'identity
+          treemacs-display-in-side-window          t
+          treemacs-eldoc-display                   'simple
+          treemacs-file-event-delay                5000
+          treemacs-file-extension-regex            treemacs-last-period-regex-value
+          treemacs-file-follow-delay               0.2
+          treemacs-file-name-transformer           #'identity
+          treemacs-follow-after-init               t
+          treemacs-expand-after-init               t
+          treemacs-find-workspace-method           'find-for-file-or-pick-first
+          treemacs-git-command-pipe                ""
+          treemacs-goto-tag-strategy               'refetch-index
+          treemacs-header-scroll-indicators        '(nil . "^^^^^^")
+          treemacs-hide-dot-git-directory          t
+          treemacs-indentation                     2
+          treemacs-indentation-string              " "
+          treemacs-is-never-other-window           nil
+          treemacs-max-git-entries                 5000
+          treemacs-missing-project-action          'ask
+          treemacs-move-forward-on-expand          nil
+          treemacs-no-png-images                   nil
+          treemacs-no-delete-other-windows         t
+          treemacs-project-follow-cleanup          nil
+          treemacs-persist-file                    (expand-file-name ".cache/treemacs-persist" user-emacs-directory)
+          treemacs-position                        'left
+          treemacs-read-string-input               'from-child-frame
+          treemacs-recenter-distance               0.1
+          treemacs-recenter-after-file-follow      nil
+          treemacs-recenter-after-tag-follow       nil
+          treemacs-recenter-after-project-jump     'always
+          treemacs-recenter-after-project-expand   'on-distance
+          treemacs-litter-directories              '("/node_modules" "/.venv" "/.cask")
+          treemacs-show-cursor                     nil
+          treemacs-show-hidden-files               t
+          treemacs-silent-filewatch                nil
+          treemacs-silent-refresh                  nil
+          treemacs-sorting                         'alphabetic-asc
+          treemacs-select-when-already-in-treemacs 'move-back
+          treemacs-space-between-root-nodes        t
+          treemacs-tag-follow-cleanup              t
+          treemacs-tag-follow-delay                1.5
+          treemacs-text-scale                      nil
+          treemacs-user-mode-line-format           nil
+          treemacs-user-header-line-format         nil
+          treemacs-wide-toggle-width               70
+          treemacs-width                           35
+          treemacs-width-increment                 1
+          treemacs-width-is-initially-locked       t
+          treemacs-workspace-switch-cleanup        nil)
+
+    ;; The default width and height of the icons is 22 pixels. If you are
+    ;; using a Hi-DPI display, uncomment this to double the icon size.
+    ;;(treemacs-resize-icons 44)
+
+    (treemacs-follow-mode t)
+    (treemacs-filewatch-mode t)
+    (treemacs-fringe-indicator-mode 'always)
+    (when treemacs-python-executable
+      (treemacs-git-commit-diff-mode t))
+
+    (pcase (cons (not (null (executable-find "git")))
+                 (not (null treemacs-python-executable)))
+      (`(t . t)
+       (treemacs-git-mode 'deferred))
+      (`(t . _)
+       (treemacs-git-mode 'simple)))
+
+    (treemacs-hide-gitignored-files-mode nil))
+  :bind
+  (:map global-map
+        ("M-0"       . treemacs-select-window)
+        ("C-x t 1"   . treemacs-delete-other-windows)
+        ("C-x t t"   . treemacs)
+        ("C-x t d"   . treemacs-select-directory)
+        ("C-x t B"   . treemacs-bookmark)
+        ("C-x t C-t" . treemacs-find-file)
+        ("C-x t M-t" . treemacs-find-tag)))
+
+(use-package treemacs-evil
+  :after (treemacs evil)
+  :ensure t)
+
+(use-package treemacs-projectile
+  :after (treemacs projectile)
+  :ensure t)
+
+(use-package treemacs-icons-dired
+  :hook (dired-mode . treemacs-icons-dired-enable-once)
+  :ensure t)
+
+(use-package treemacs-magit
+  :after (treemacs magit)
+  :ensure t)
+
+(use-package treemacs-persp ;;treemacs-perspective if you use perspective.el vs. persp-mode
+  :after (treemacs persp-mode) ;;or perspective vs. persp-mode
+  :ensure t
+  :config (treemacs-set-scope-type 'Perspectives))
+
+(use-package treemacs-tab-bar ;;treemacs-tab-bar if you use tab-bar-mode
+  :after (treemacs)
+  :ensure t
+  :config (treemacs-set-scope-type 'Tabs))
+;;;; treemacs installation finished
 
 ;;; This will enable emacs to compile a simple cpp single file without any makefile by just pressing [f9] key
 (defun code-compile()
@@ -145,6 +219,7 @@
 
 (global-set-key [f9] 'code-compile)
 
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -152,7 +227,7 @@
  ;; If there is more than one, they won't work right.
  '(ispell-dictionary nil)
  '(package-selected-packages
-   '(lsp-mode dap-java projectile lsp-java dap-mode flycheck company-quickhelp ccls company-lsp quickrun which-key material-theme use-package)))
+   '(treemacs-persp treemacs-tab-bar helm-lsp lsp-java hydra yasnippet projectile quickrun ccls lsp-ui lsp-mode company-quickhelp material-theme which-key flycheck exec-path-from-shell use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
